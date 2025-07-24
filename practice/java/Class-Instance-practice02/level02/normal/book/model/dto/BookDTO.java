b0fdd74ae2256c85ebecfc07a89bf3590f32506b
nayeon730 <dldsk01512@gmail.com>
2025-07-24 18:13:18 +0900

    BookDTO

--- practice/java/Class-Instance-practice02/level02/normal/book/model/dto/BookDTO.java
+++ practice/java/Class-Instance-practice02/level02/normal/book/model/dto/BookDTO.java
@@ -16,4 +16,83 @@ public class BookDTO {
 //
 //+ printInformation():void       // 필드값 출력용 메소드
 
+
+    private String title;
+    private String publisher;
+    private String author;
+    private int price;
+    private double discountRate;
+
+
+
+    /* tt. 생성자 */
+    public BookDTO()  {}
+
+    public BookDTO(String title, String publisher, String author) {
+        this.title = title;
+        this.publisher = publisher;
+        this.author = author;
+    }
+
+    public BookDTO(String title, String publisher, String author, int price, double discountRate) {
+        this(title, publisher, author);   /* tt. 위에 미리 작성했기 때문에 전달 */
+        this.price = price;
+        this.discountRate = discountRate;
+    }
+
+
+    /* tt. setter */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+
+    /* tt. getter */
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+
+
+    /* tt. 필드값 출력용 메소드 */
+    public String printInformation() {
+
+        return title + ", " +
+                publisher + ", " +
+                author + ", " +
+                price + ", " +
+                discountRate;
+    }
 }
--- practice/java/Class-Instance-practice02/level02/normal/book/run/Application.java
+++ practice/java/Class-Instance-practice02/level02/normal/book/run/Application.java
@@ -1,4 +1,68 @@
 package com.ohgiraffers.practice02.level02.normal.book.run;
 
+import com.ohgiraffers.practice02.level02.normal.book.model.dto.BookDTO;
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.print.Book;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        BookDTO bookDTO = new BookDTO();
+
+        /* tt. 기본 생성자 호출 */
+        System.out.println(bookDTO.printInformation());     // null, null, 0, 0.0
+
+        /* tt. title, publisher, author를 매개변수로 전달받는 생성자 호출 */
+        BookDTO bookDTO2 = new BookDTO("자바의 정석", "도우출판", "남궁성");
+        System.out.println(bookDTO2.printInformation());    // 자바의 정석, 도우출판, 남궁성, 0, 0.0
+
+        /* tt. 모든 필드를 초기화하는 생성자 호출 */
+        BookDTO bookDTO3 = new BookDTO("홍길동전", "활빈당", "허균", 500000, 0.5);
+        System.out.println(bookDTO3.printInformation());    // 홍길동전, 활빈당, 허균, 5000000, 0.5
+
+        System.out.println();
+
+        /* tt. getter & setter */
+        BookDTO bookDTO4 = new BookDTO();
+
+        System.out.println(bookDTO4.getTitle());
+        System.out.println(bookDTO4.getPublisher());
+        System.out.println(bookDTO4.getAuthor());
+        System.out.println(bookDTO4.getPrice());
+        System.out.println(bookDTO4.getDiscountRate());
+
+        System.out.println();
+        //
+
+        BookDTO bookDTO5 = new BookDTO();
+        bookDTO5.setTitle("자바의 정석");
+        bookDTO5.setPublisher("도우출판");
+        bookDTO5.setAuthor("남궁성");
+
+        System.out.println(bookDTO5.getTitle());
+        System.out.println(bookDTO5.getPublisher());
+        System.out.println(bookDTO5.getAuthor());
+        System.out.println(bookDTO5.getPrice());
+        System.out.println(bookDTO5.getDiscountRate());
+
+        System.out.println();
+        //
+
+        BookDTO bookDTO6 = new BookDTO();
+        bookDTO6.setTitle("홍길동전");
+        bookDTO6.setPublisher("활빈당");
+        bookDTO6.setAuthor("허균");
+        bookDTO6.setPrice(500000);
+        bookDTO6.setDiscountRate(0.5);
+
+        System.out.println(bookDTO6.getTitle());
+        System.out.println(bookDTO6.getPublisher());
+        System.out.println(bookDTO6.getAuthor());
+        System.out.println(bookDTO6.getPrice());
+        System.out.println(bookDTO6.getDiscountRate());
+    }
+
+
 }
